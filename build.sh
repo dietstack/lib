68505b466884e355740ee50c6343b44780eff638
Kamil Madac <kamil.madac@gmail.com>
2018-01-12 10:16:29 +0100

    Branch is added to image name if != master

--- build.sh
+++ build.sh
@@ -10,10 +10,18 @@ DOCKER_PROJ_NAME=${DOCKER_PROJ_NAME:-''}
 
 # set version based on the git commit
 VERSION=$(git describe --abbrev=7 --tags)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+TAG_BRANCH=""
+if [ ${BRANCH} != "master" ]; then
+    TAG_BRANCH=${BRANCH}-
+fi
+
+FULL_VERSION=${TAG_BRANCH}${VERSION}
 
 docker build --build-arg http_proxy=${http_proxy:-} \
        --build-arg https_proxy=${https_proxy:-} \
        --build-arg no_proxy=${no_proxy:-} \
-       $@ -t ${DOCKER_PROJ_NAME}${APP_NAME}:${VERSION} .
+       $@ -t ${DOCKER_PROJ_NAME}${APP_NAME}:${FULL_VERSION} .
 
-docker tag ${DOCKER_PROJ_NAME}${APP_NAME}:${VERSION} ${DOCKER_PROJ_NAME}${APP_NAME}:latest
+docker tag ${DOCKER_PROJ_NAME}${APP_NAME}:${FULL_VERSION} ${DOCKER_PROJ_NAME}${APP_NAME}:latest
