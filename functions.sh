e6235b7092f79089afdc9b26c17a9310cfa274dc
Kamil Madac <kamil.madac@gmail.com>
2018-03-21 23:35:01 +0100

    Added checks whether DB exists and create a db only if db doesn't exists

--- functions.sh
+++ functions.sh
@@ -19,15 +19,31 @@ wait_for_port() {
     fi
 }
 
+db_exists() {
+    # prints 0 if db doesnt exists
+    # prints >0 if db exists
+    local DB_NAME=$1
+    local USER_NAME=$2
+    local ROOT_DB_PASSWD=$3
+    MYSQL_CMD="docker run --rm --net=host ${DOCKER_PROJ_NAME}osadmin mysql -h 127.0.0.1 -P 3306 -u root -p$ROOT_DB_PASSWD"
+    $MYSQL_CMD -e "SHOW DATABASES" | grep -w $DB_NAME | wc -l 
+}
+
 create_db_osadmin() {
     # Usage: create_db_osadmin keystone keystone veryS3cr3t veryS3cr3t
     local DB_NAME=$1
     local USER_NAME=$2
     local ROOT_DB_PASSWD=$3
     local SVC_DB_PASSWD=$4
-    echo "Creating $DB_NAME database ..."
     MYSQL_CMD="docker run --rm --net=host ${DOCKER_PROJ_NAME}osadmin mysql -h 127.0.0.1 -P 3306 -u root -p$ROOT_DB_PASSWD"
-    $MYSQL_CMD -e "CREATE DATABASE $DB_NAME;"
-    $MYSQL_CMD -e "CREATE USER '$USER_NAME'@'%' IDENTIFIED BY '$SVC_DB_PASSWD';"
-    $MYSQL_CMD -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER_NAME'@'%' WITH GRANT OPTION;"
+    if [[ "$(db_exists $DB_NAME $USER_NAME $ROOT_DB_PASSWD)" == 0 ]]; then
+        echo "Creating $DB_NAME database ..."
+        $MYSQL_CMD -e "CREATE DATABASE $DB_NAME;"
+        $MYSQL_CMD -e "CREATE USER '$USER_NAME'@'%' IDENTIFIED BY '$SVC_DB_PASSWD';"
+        $MYSQL_CMD -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER_NAME'@'%' WITH GRANT OPTION;"
+    else
+        echo "Database already exists."
+    fi
 }
+
+
